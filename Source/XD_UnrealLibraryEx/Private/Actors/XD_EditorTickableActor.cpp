9ce61a98d2bdf77a4de311e1ae323bdae186d642
Eragon-Brisingr <450614754@qq.com>
2018-09-11 21:24:15 +0800

    添加了可以在编辑器里Tick的Actor

---
+++ Source/XD_UnrealLibraryEx/Private/Actors/XD_EditorTickableActor.cpp
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "XD_EditorTickableActor.h"
+
+
+// Sets default values
+AXD_EditorTickableActor::AXD_EditorTickableActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AXD_EditorTickableActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AXD_EditorTickableActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AXD_EditorTickableActor::TickActor(float DeltaSeconds, ELevelTick TickType, FActorTickFunction& ThisTickFunction)
+{
+	FEditorScriptExecutionGuard ScriptGuard;
+	Super::TickActor(DeltaSeconds, TickType, ThisTickFunction);
+}
+
+AXD_OnlyEditorTickableActor::AXD_OnlyEditorTickableActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AXD_OnlyEditorTickableActor::Tick(float DeltaTime)
+{
+	if (GetWorld()->WorldType == EWorldType::Editor)
+	{
+		Super::Tick(DeltaTime);
+	}
+}
+
+void AXD_OnlyEditorTickableActor::TickActor(float DeltaSeconds, ELevelTick TickType, FActorTickFunction& ThisTickFunction)
+{
+	FEditorScriptExecutionGuard ScriptGuard;
+	Super::TickActor(DeltaSeconds, TickType, ThisTickFunction);
+}
---
+++ Source/XD_UnrealLibraryEx/Public/Actors/XD_EditorTickableActor.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "XD_EditorTickableActor.generated.h"
+
+UCLASS(abstract)
+class XD_UNREALLIBRARYEX_API AXD_EditorTickableActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AXD_EditorTickableActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual bool ShouldTickIfViewportsOnly() const override { return true; }
+	virtual void TickActor(float DeltaSeconds, ELevelTick TickType, FActorTickFunction& ThisTickFunction) override;
+	
+};
+
+UCLASS(abstract)
+class XD_UNREALLIBRARYEX_API AXD_OnlyEditorTickableActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AXD_OnlyEditorTickableActor();
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual bool ShouldTickIfViewportsOnly() const override { return true; }
+	virtual void TickActor(float DeltaSeconds, ELevelTick TickType, FActorTickFunction& ThisTickFunction) override;
+};
+
