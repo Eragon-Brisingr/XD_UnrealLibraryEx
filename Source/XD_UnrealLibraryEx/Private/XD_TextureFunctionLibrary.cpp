fd55c4236db4107de15ecff4ce720b2cf07fa66e
Eragon-Brisingr <450614754@qq.com>
2018-09-19 20:36:39 +0800

    添加函数库

---
+++ Source/XD_UnrealLibraryEx/Private/XD_TextureFunctionLibrary.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "XD_TextureFunctionLibrary.h"
+#include <Engine/Texture2D.h>
+#include <TextureResource.h>
+#include <BulkData.h>
+#include "XD_UnrealLibraryExUtility.h"
+#include <Engine/TextureRenderTarget2D.h>
+
---
+++ Source/XD_UnrealLibraryEx/Public/XD_TextureFunctionLibrary.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "XD_TextureFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XD_UNREALLIBRARYEX_API UXD_TextureFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+
+};
