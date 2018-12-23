d300e1064c3e3c77241408f7e90527351922df71
Eragon <eragon.xuduo@gmail.com>
2018-12-23 16:24:55 +0800

    修正无法使用前向声明的问题

--- Source/XD_UnrealLibraryEx/Public/XD_MacrosLibrary.h
+++ Source/XD_UnrealLibraryEx/Public/XD_MacrosLibrary.h
@@ -11,7 +11,7 @@
 struct XD_UNREALLIBRARYEX_API XD_Macros_Helper
 {
 	//允许前向声明
-	template<typename T, unsigned long long Length>
+	template<typename T, bool IsCppName, unsigned long long Length>
 	static constexpr const char* GetTypeName(const char(&Type)[Length])
 	{
 		static_assert(Length > 0, "Type name must not empty !");
@@ -22,9 +22,9 @@ struct XD_UNREALLIBRARYEX_API XD_Macros_Helper
 #define ClassForwardDeclaration_Length sizeof(ClassForwardDeclaration) - 1
 #define StructForwardDeclaration_Length sizeof(StructForwardDeclaration) - 1
 
-		return Length > ClassForwardDeclaration_Length && StringCompare(ClassForwardDeclaration_Length, Type, ClassForwardDeclaration) ? GetTypeNameImpl(Type) + ClassForwardDeclaration_Length :
-			Length > StructForwardDeclaration_Length && StringCompare(StructForwardDeclaration_Length, Type, StructForwardDeclaration) ? GetTypeNameImpl(Type) + StructForwardDeclaration_Length :
-			GetTypeNameImpl(Type);
+		return Length > ClassForwardDeclaration_Length && StringCompare(ClassForwardDeclaration_Length, Type, ClassForwardDeclaration) ? GetTypeNameImpl(Type, IsCppName) + ClassForwardDeclaration_Length :
+			Length > StructForwardDeclaration_Length && StringCompare(StructForwardDeclaration_Length, Type, StructForwardDeclaration) ? GetTypeNameImpl(Type, IsCppName) + StructForwardDeclaration_Length :
+			GetTypeNameImpl(Type, IsCppName);
 
 #undef ClassForwardDeclaration_Length
 #undef StructForwardDeclaration_Length
@@ -35,9 +35,9 @@ struct XD_UNREALLIBRARYEX_API XD_Macros_Helper
 
 private:
 	//由于虚幻命名规则，向后移动一个字符才是类型的名字
-	static constexpr const char* GetTypeNameImpl(const char* Type)
+	static constexpr const char* GetTypeNameImpl(const char* Type, bool IsCppName) 
 	{
-		return Type + 1;
+		return IsCppName ? Type : Type + 1;
 	}
 
 	static constexpr bool StringCompare(const long long CompareLength, const char* LHS, const char* RHS)
@@ -52,16 +52,16 @@ private:
 };
 
 //获取类型名，允许前向声明
-#define GET_TYPE_NAME_STRING_CHECKED(Type) FString(XD_Macros_Helper::GetTypeName<Type>(#Type))
+#define GET_TYPE_NAME_STRING_CHECKED(Type) FString(XD_Macros_Helper::GetTypeName<Type, false>(#Type))
 
 //获取类型名，允许前向声明
-#define GET_TYPE_NAME_CHECKED(Type) FName(XD_Macros_Helper::GetTypeName<Type>(#Type))
+#define GET_TYPE_NAME_CHECKED(Type) FName(XD_Macros_Helper::GetTypeName<Type, false>(#Type))
 
 //获取c++中定义时的类型名，允许前向声明
-#define GET_TYPE_CPP_NAME_STRING_CHECKED(Type) (sizeof(Type), FString(TEXT(#Type)))
+#define GET_TYPE_CPP_NAME_STRING_CHECKED(Type) FString(XD_Macros_Helper::GetTypeName<Type, true>(#Type))
 
 //获取c++中定义时的类型名，允许前向声明
-#define GET_TYPE_CPP_NAME_CHECKED(Type) FName(*GET_TYPE_CPP_NAME_STRING_CHECKED(Type))
+#define GET_TYPE_CPP_NAME_CHECKED(Type) FName(XD_Macros_Helper::GetTypeName<Type, true>(#Type))
 
 //用来查看私有变量的偏移地址
 #define LookMemberOffset(type, member) (size_t)&(((type*)0)->member)
